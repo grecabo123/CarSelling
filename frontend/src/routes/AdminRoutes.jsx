b7646e99d120fcdd4b79082fd88626a3a9312def
GCodingIsLife <artamay1@gmail.com>
2024-01-04 21:19:10 +0800

    sellls

--- frontend/src/routes/AdminRoutes.jsx
+++ frontend/src/routes/AdminRoutes.jsx
@@ -3,6 +3,7 @@ import Logs from "../components/Admin/pages/ActivityLogs/Logs";
 import Dashboard from "../components/Admin/pages/Dashboard";
 import Sell from "../components/Admin/pages/Products/Sell";
 import TransactionHistory from "../components/Admin/pages/Transaction/TransactionHistory";
+import Sells from "../components/Admin/pages/View/Sells";
 
 
 const AdminRoutes = [
@@ -11,6 +12,7 @@ const AdminRoutes = [
     {path: "/admin/AddProduct", exact: true, name: "Add", component: Sell },
     {path: "/admin/logs", exact: true, name: "Add", component: Logs },
     {path: "/admin/transaction", exact: true, name: "Add", component: TransactionHistory },
+    {path: "/admin/sell", exact: true, name: "Add", component: Sells },
 ];
 
 export default AdminRoutes;
